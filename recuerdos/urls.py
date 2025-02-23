f5d1222fde95767a8ad93985d3f4d48b101929e4
JorgeAndresPinaAngel <jorg.pina@duocuc.cl>
2025-02-23 20:02:23 -0300

    wipe

--- recuerdos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.carta, name='carta'),
-    path('recuerdos/', views.recuerdos, name='recuerdos'),
-    path('fotos/', views.fotos, name='fotos'),
-    path('crear_recuerdo/', views.crear_recuerdo, name='crear_recuerdo'),
-]
